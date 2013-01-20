a486123413686de1fa97ef00d057f237c17a01e8
w8r <alexander.milewski@gmail.com>
2013-01-20 16:39:20 +0100

    upload 2013.01.20_16-39-20

--- vendor/tagmanager/bootstrap-tagmanager.js
+++ vendor/tagmanager/bootstrap-tagmanager.js
@@ -500,6 +500,7 @@
             }
 
             if (tagManagerOptions.prefilled != null) {
+                console.log(tagManagerOptions.prefilled);
                 if (typeof(tagManagerOptions.prefilled) == "object") {
                     var pta = tagManagerOptions.prefilled;
                     $.each(pta, function(key, val) {
