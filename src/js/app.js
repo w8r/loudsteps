3dee03d5a747553f09abde6740f1b9f0a5d71ab0
w8r <alexander.milewski@gmail.com>
2013-01-20 03:45:36 +0100

    upload 2013.01.20_03-45-36

--- src/js/app.js
+++ src/js/app.js
@@ -196,14 +196,19 @@
                 poi = POIs[i];
                 icon = poi.categories[0].icon;
                 size = parseInt(icon.sizes[0]);
+                console.log({
+                            url : icon.prefix + size + icon.name,
+                            size : new google.maps.Size(size, size),
+                            origin : new google.maps.Point(-size / 2, -size / 2)
+                        });
                 marker = new google.maps.Marker({
                             map : this.map,
-//                            icon : {
-//                                url : icon.prefix + size + icon.name,
-//                                size : new google.maps.Size(size, size),
-//                                origin : new google.maps.Point(-size / 2, -size
-//                                                / 2)
-//                            },
+                            // icon : {
+                            // url : icon.prefix + size + icon.name,
+                            // size : new google.maps.Size(size, size),
+                            // origin : new google.maps.Point(-size / 2, -size
+                            // / 2)
+                            // },
                             position : new google.maps.LatLng(poi.location.lat,
                                     poi.location.lng)
                         });
