4ce40c6b4020385bdf1e8cb829cf9eb51dca6ba8
w8r <alexander.milewski@gmail.com>
2013-01-20 03:45:06 +0100

    upload 2013.01.20_03-45-06

--- src/js/app.js
+++ src/js/app.js
@@ -198,12 +198,12 @@
                 size = parseInt(icon.sizes[0]);
                 marker = new google.maps.Marker({
                             map : this.map,
-                            icon : {
-                                url : icon.prefix + size + icon.name,
-                                size : new google.maps.Size(size, size),
-                                origin : new google.maps.Point(-size / 2, -size
-                                                / 2)
-                            },
+//                            icon : {
+//                                url : icon.prefix + size + icon.name,
+//                                size : new google.maps.Size(size, size),
+//                                origin : new google.maps.Point(-size / 2, -size
+//                                                / 2)
+//                            },
                             position : new google.maps.LatLng(poi.location.lat,
                                     poi.location.lng)
                         });
